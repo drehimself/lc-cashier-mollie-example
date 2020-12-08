9abdeafdfb2e7badc6813cc7e6d2c07b7730334c
Andre Madarang <andre_madarang@hotmail.com>
2020-12-08 00:38:24 -0500

    Example

---
+++ app/Models/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
+use Laravel\Cashier\Order\Contracts\ProvidesInvoiceInformation;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable implements ProvidesInvoiceInformation
+{
+    use Billable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    /**
+     * Get the receiver information for the invoice.
+     * Typically includes the name and some sort of (E-mail/physical) address.
+     *
+     * @return array An array of strings
+     */
+    public function getInvoiceInformation()
+    {
+        return [$this->name, $this->email];
+    }
+
+    /**
+     * Get additional information to be displayed on the invoice. Typically a note provided by the customer.
+     *
+     * @return string|null
+     */
+    public function getExtraBillingInformation()
+    {
+        return null;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Laravel\Cashier\SubscriptionBuilder\RedirectToCheckoutResponse;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/subscribe', function () {
+    return view('subscribe');
+})->name('subscribe');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/coupon', function () {
+    auth()->user()->redeemCoupon('welcome', 'main');
+
+    return redirect('/dashboard')->with('status', 'Coupon redeemed');
+})->name('coupon');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/subscribe', function (Request $request) {
+    $user = auth()->user();
+
+    $name = 'main';
+    $plan = $request->plan;
+
+    if (!$user->subscribed($name, $plan)) {
+
+        $result = $user->newSubscription($name, $plan)->create();
+
+        if (is_a($result, RedirectToCheckoutResponse::class)) {
+            return $result; // Redirect to Mollie checkout
+        }
+
+        return redirect('/dashboard')->with('status', 'Welcome to the ' . $plan . ' plan');
+    }
+
+    return redirect('/dashboard')->with('status', 'You are already on the ' . $plan . ' plan');
+})->name('subscribe.post');
